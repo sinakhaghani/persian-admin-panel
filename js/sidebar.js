9f8ea65131fe759d283bc95273689ad78a930b7b
sina.it91 <sina.it91@gmail.com>
2022-12-18 12:54:35 +0330

    create base template

---
+++ js/sidebar.js
@@ -0,0 +1,10 @@
+function openNav() {
+    document.getElementById("mySidebar").style.width = "250px";
+    document.getElementById("main").style.marginRight = "250px";
+}
+
+function closeNav() {
+    document.getElementById("mySidebar").style.width = "0";
+    document.getElementById("main").style.marginRight= "0";
+    document.getElementById("main").style.marginRight= "0";
+}
